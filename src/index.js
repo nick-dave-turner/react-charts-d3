08286cf91fa9bfa32543b08fc52c1bdc04c9d97d
nick-dave-turner <37630270+nick-dave-turner@users.noreply.github.com>
2018-04-07 16:11:47 +0100

    expose bubblechart to library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+export { default as AreaChart } from './components/AreaChart/AreaChart';
 export { default as BarChart } from './components/BarChart/BarChart';
+export { default as BubbleChart } from './components/BubbleChart/BubbleChart';
 export { default as LineChart } from './components/LineChart/LineChart';
 export {
   default as ScatterChart,
 } from './components/ScatterChart/ScatterChart';
-export { default as AreaChart } from './components/AreaChart/AreaChart';
